f011c315fad78730cb1f585cd80718f0f9194512
Xue Jinyu <30921318+titxue@users.noreply.github.com>
2023-04-21 05:26:14 +0000

    bug 无法打包

--- src/App.vue
+++ src/App.vue
@@ -3,56 +3,56 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-
+import TableC from "./components/TableC.vue";
 import { ref } from 'vue'
 import dayjs from 'dayjs'
 
 const now = new Date()
 
 const tableData = ref([
-    {
-        date: '2016-05-01',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-    },
-    {
-        date: '2016-05-02',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-    },
-    {
-        date: '2016-05-03',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-    },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
 ])
 
 const deleteRow = (index: number) => {
-    tableData.value.splice(index, 1)
+  tableData.value.splice(index, 1)
 }
 
 const onAddItem = () => {
-    now.setDate(now.getDate() + 1)
-    tableData.value.push({
-        date: dayjs(now).format('YYYY-MM-DD'),
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-    })
+  now.setDate(now.getDate() + 1)
+  tableData.value.push({
+    date: dayjs(now).format('YYYY-MM-DD'),
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  })
 }
 
-interface Scope { row: Record<string, unknown>; $index: number; }
+
 
 </script>
 
@@ -69,32 +69,8 @@ interface Scope { row: Record<string, unknown>; $index: number; }
 
   <el-button>Default</el-button>
 
-    <el-table :data="tableData" style="width: 100%" max-height="250">
-        <el-table-column fixed prop="date" label="Date" width="150" />
-        <el-table-column prop="name" label="Name" width="120" />
-        <el-table-column prop="state" label="State" width="120" />
-        <el-table-column prop="city" label="City" width="120" />
-        <el-table-column prop="address" label="Address" width="600" />
-        <el-table-column prop="zip" label="Zip" width="120" />
-        <el-table-column fixed="right" label="Operations" width="120">
-            <!-- #default="scope:any" -->
-            <!-- #default="scope:Scope" -->
-            <template #default="scope">
-                <el-button
-                        link
-                        type="primary"
-                        size="small"
-                        @click.prevent="deleteRow(scope.$index)"
-                >
-                    Remove
-                </el-button>
-            </template>
-        </el-table-column>
-    </el-table>
-    <el-button class="mt-4" style="width: 100%" @click="onAddItem"
-    >Add Item</el-button
-    >
-
+  <table-c :data="tableData" @delete-row="deleteRow" />
+  <el-button class="mt-4" style="width: 100%" @click="onAddItem">Add Item</el-button>
 </template>
 
 <style scoped>
@@ -103,9 +79,11 @@ interface Scope { row: Record<string, unknown>; $index: number; }
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
